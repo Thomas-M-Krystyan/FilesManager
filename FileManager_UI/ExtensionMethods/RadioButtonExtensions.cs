b0a69654484dd7d98e962e7f2aa36e32debf8b61
Tomasz M. Krystyan <neo.dark.lord@gmail.com>
2022-06-10 17:16:13 +0200

    Add radio button extensions

---
+++ FileManager_UI/ExtensionMethods/RadioButtonExtensions.cs
@@ -0,0 +1,41 @@
+﻿using System.Windows.Controls;
+
+namespace FileManager_UI.ExtensionMethods
+{
+    public static class RadioButtonExtensions
+    {
+        /// <summary>
+        /// Checks given <see cref="RadioButton"/>.
+        /// </summary>
+        public static void Activate(this RadioButton radioButton)
+        {
+            radioButton.IsChecked = true;
+        }
+
+        /// <summary>
+        /// Checks given <see cref="RadioButton"/>.
+        /// </summary>
+        public static void Activate(this RadioButton radioButton, ref bool isSelected)
+        {
+            radioButton.Activate();
+            isSelected = true;
+        }
+
+        /// <summary>
+        /// Unchecks given <see cref="RadioButton"/>.
+        /// </summary>
+        public static void Deactivate(this RadioButton radioButton)
+        {
+            radioButton.IsChecked = false;
+        }
+
+        /// <summary>
+        /// Unchecks given <see cref="RadioButton"/>.
+        /// </summary>
+        public static void Deactivate(this RadioButton radioButton, ref bool isSelected)
+        {
+            radioButton.Deactivate();
+            isSelected = false;
+        }
+    }
+}
