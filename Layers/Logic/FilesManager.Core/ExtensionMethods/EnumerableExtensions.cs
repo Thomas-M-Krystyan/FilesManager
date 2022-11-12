628b0071c5946e929bde35e6deb028b779f87ea0
Tomasz M. Krystyan <neo.dark.lord@gmail.com>
2022-11-12 07:38:55 +0100

    New extension methods class

---
+++ Layers/Logic/FilesManager.Core/ExtensionMethods/EnumerableExtensions.cs
@@ -0,0 +1,13 @@
+﻿namespace FilesManager.Core.ExtensionMethods
+{
+    /// <summary>
+    /// Extension methods for <see cref="IEnumerable{T}"/>s.
+    /// </summary>
+    internal static class EnumerableExtensions
+    {
+        internal static bool IsNullOrEmpty<T>(this IEnumerable<T> collection)
+        {
+            return collection?.Count() > 0;
+        }
+    }
+}
