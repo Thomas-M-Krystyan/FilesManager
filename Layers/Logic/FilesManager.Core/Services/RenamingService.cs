5573bf9d97938982aaa7b39e35a646f900109d3e
Tomasz M. Krystyan <neo.dark.lord@gmail.com>
2023-02-18 23:24:50 +0100

    Move saving logic into a dedicated WritingService class

--- Layers/Logic/FilesManager.Core/Services/RenamingService.cs
+++ Layers/Logic/FilesManager.Core/Services/RenamingService.cs
@@ -1,4 +1,5 @@
 using FilesManager.Core.DTOs;
+using FilesManager.Core.Services;
 using FilesManager.Core.Services.Strategies;
 using FilesManager.Core.Validation;
 
@@ -11,7 +12,7 @@ namespace FileManager.Layers.Logic
         /// </summary>
         public static RenamingResultDto ReplaceWithNumber(string oldFilePath, string prefix, ushort number, string postfix)
         {
-            return RenameFile(oldFilePath, () => Increment.GetNumberIncrementedName(oldFilePath, prefix, number, postfix));
+            return WritingService.RenameFile(oldFilePath, () => Increment.GetNumberIncrementedName(oldFilePath, prefix, number, postfix));
         }
 
         /// <summary>
@@ -25,7 +26,7 @@ namespace FileManager.Layers.Logic
         /// </summary>
         public static RenamingResultDto EnrichWithPrependAndAppend(string oldFilePath, string textToPrepend, string textToAppend)
         {
-            return RenameFile(oldFilePath, () => PrependAppend.GetPrependedAndAppendedName(oldFilePath, textToPrepend, textToAppend));
+            return WritingService.RenameFile(oldFilePath, () => PrependAppend.GetPrependedAndAppendedName(oldFilePath, textToPrepend, textToAppend));
         }
 
         /// <summary>
@@ -36,32 +37,8 @@ namespace FileManager.Layers.Logic
             RenamingResultDto result = Validate.IsPathDtoValid(fileNameComponents, fileNameComponents.FullName);
 
             return result.IsSuccess
-                ? RenameFile(oldFilePath, () => LeadingZeros.GetLeadedZerosName(fileNameComponents, zerosCount, maxNumberLength))
+                ? WritingService.RenameFile(oldFilePath, () => LeadingZeros.GetLeadedZerosName(fileNameComponents, zerosCount, maxNumberLength))
                 : result;  // Failure
         }
-
-        #region System.IO.File
-        /// <summary>
-        /// Renames the given file using a specified renaming method.
-        /// </summary>
-        /// <param name="oldFilePath">The original file path.</param>
-        /// <param name="renameMethod">The rename method to be used.</param>
-        /// <returns>Result of renamed file.</returns>
-        private static RenamingResultDto RenameFile(string oldFilePath, Func<string> renameMethod)
-        {
-            try
-            {
-                string newFilePath = renameMethod();
-
-                File.Move(oldFilePath, newFilePath);
-
-                return RenamingResultDto.Success(newFilePath);
-            }
-            catch (Exception exception)
-            {
-                return RenamingResultDto.Failure(exception.Message);
-            }
-        }
-        #endregion
     }
 }
---
+++ Layers/Logic/FilesManager.Core/Services/WritingService.cs
@@ -0,0 +1,29 @@
+﻿using FilesManager.Core.DTOs;
+
+namespace FilesManager.Core.Services
+{
+    internal static class WritingService
+    {
+        /// <summary>
+        /// Renames the given file using a specified renaming method.
+        /// </summary>
+        /// <param name="oldFilePath">The original file path.</param>
+        /// <param name="renameMethod">The rename method to be used.</param>
+        /// <returns>Result of renamed file.</returns>
+        internal static RenamingResultDto RenameFile(string oldFilePath, Func<string> renameMethod)
+        {
+            try
+            {
+                string newFilePath = renameMethod();
+
+                File.Move(oldFilePath, newFilePath);
+
+                return RenamingResultDto.Success(newFilePath);
+            }
+            catch (Exception exception)
+            {
+                return RenamingResultDto.Failure(exception.Message);
+            }
+        }
+    }
+}
