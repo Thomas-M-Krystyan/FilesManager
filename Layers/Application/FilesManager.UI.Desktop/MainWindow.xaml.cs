9de77e0ae2fcf47bc533197a797075dec2cd9e1c
Tomasz M. Krystyan <neo.dark.lord@gmail.com>
2023-07-20 01:10:28 +0200

    Speed up ToolTips show delay

--- Layers/Application/FilesManager.UI.Desktop/MainWindow.xaml.cs
+++ Layers/Application/FilesManager.UI.Desktop/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using FilesManager.Core.Validation;
 using FilesManager.UI.Desktop.ExtensionMethods;
 using FilesManager.UI.Desktop.Utilities;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
