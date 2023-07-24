041cc5720402d44d384f3b17604f8e1be6fe3a2e
Tomasz M. Krystyan <neo.dark.lord@gmail.com>
2023-07-24 02:05:00 +0200

    Add ViewModelBase

---
+++ Layers/Application/FilesManager.UI.Desktop/ViewModels/ViewModelBase.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel;
+
+namespace FilesManager.UI.Desktop.ViewModels
+{
+    internal abstract class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
