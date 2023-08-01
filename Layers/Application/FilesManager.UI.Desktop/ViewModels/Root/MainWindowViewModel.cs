70e8e65a5ba45e16755691cfd0c86c00500d0e7d
Tomasz M. Krystyan <neo.dark.lord@gmail.com>
2023-08-01 22:50:05 +0200

    Initiating main view model

---
+++ Layers/Application/FilesManager.UI.Desktop/ViewModels/Root/MainWindowViewModel.cs
@@ -0,0 +1,14 @@
+﻿using FilesManager.UI.Desktop.ViewModels.Strategies;
+
+namespace FilesManager.UI.Desktop.ViewModels.Root
+{
+    internal sealed class MainWindowViewModel : ViewModelBase
+    {
+        public IncrementNumberViewModel IncrementStrategy { get; set; }
+
+        internal MainWindowViewModel()
+        {
+            this.IncrementStrategy = new IncrementNumberViewModel();
+        }
+    }
+}
