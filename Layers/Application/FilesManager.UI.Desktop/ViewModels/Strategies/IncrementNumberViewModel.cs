2effd357cdf02f1b97bd89b26b4de39893d11620
Tomasz M. Krystyan <neo.dark.lord@gmail.com>
2023-08-01 23:12:40 +0200

    Introduce IncrementNumberViewModel

---
+++ Layers/Application/FilesManager.UI.Desktop/ViewModels/Strategies/IncrementNumberViewModel.cs
@@ -0,0 +1,15 @@
+﻿using FilesManager.UI.Desktop.Properties;
+using System;
+
+namespace FilesManager.UI.Desktop.ViewModels.Strategies
+{
+    internal sealed class IncrementNumberViewModel : ViewModelBase
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="IncrementNumberViewModel"/> class.
+        /// </summary>
+        internal IncrementNumberViewModel()
+        {
+        }
+    }
+}
