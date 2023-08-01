2ed1ae7eb21a543d1e77c48d6dbf90a675bcf9d7
Tomasz M. Krystyan <neo.dark.lord@gmail.com>
2023-08-01 23:22:02 +0200

    Documentation

--- Layers/Application/FilesManager.UI.Desktop/ViewModels/Root/MainWindowViewModel.cs
+++ Layers/Application/FilesManager.UI.Desktop/ViewModels/Root/MainWindowViewModel.cs
@@ -4,8 +4,13 @@ namespace FilesManager.UI.Desktop.ViewModels.Root
 {
     internal sealed class MainWindowViewModel : ViewModelBase
     {
+        #region View Models
         public IncrementNumberViewModel IncrementStrategy { get; set; }
+        #endregion
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MainWindowViewModel"/> class.
+        /// </summary>
         internal MainWindowViewModel()
         {
             this.IncrementStrategy = new IncrementNumberViewModel();
--- Layers/Application/FilesManager.UI.Desktop/ViewModels/ViewModelBase.cs
+++ Layers/Application/FilesManager.UI.Desktop/ViewModels/ViewModelBase.cs
@@ -2,10 +2,16 @@
 
 namespace FilesManager.UI.Desktop.ViewModels
 {
+    /// <summary>
+    /// Base class for all view models in MVVM architecture.
+    /// </summary>
     internal abstract class ViewModelBase : INotifyPropertyChanged
     {
         public event PropertyChangedEventHandler? PropertyChanged;
 
+        /// <summary>
+        /// Notifies a specific view model whether its property was changed from XAML code.
+        /// </summary>
         protected void OnPropertyChanged(string propertyName)
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
