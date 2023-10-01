90a31157e1df2d3bd91f5255b4de93d03e12c681
Tomasz M. Krystyan <neo.dark.lord@gmail.com>
2023-10-01 14:28:44 +0200

    Move FileData POCO to Logic project

---
+++ Layers/Logic/FilesManager.Core/Models/POCOs/FileData.cs
@@ -0,0 +1,23 @@
+﻿namespace FilesManager.Core.Models.POCOs
+{
+    /// <summary>
+    /// The POCO model representing <see cref="File"/> metadata.
+    /// </summary>
+    public record FileData
+    {
+        /// <summary>
+        /// The full path to the file: <code>"C:\Users\JaneDoe\Desktop\Test.txt"</code>
+        /// </summary>
+        public string Path { get; set; } = string.Empty;
+
+        /// <summary>
+        /// The name of the file: <code>"Test"</code>
+        /// </summary>
+        public string Name { get; set; } = string.Empty;
+
+        /// <summary>
+        /// The extension of the file: <code>".txt"</code>
+        /// </summary>
+        public string Extension { get; set; } = string.Empty;
+    }
+}
